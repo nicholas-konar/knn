f5f8d4472780905527581f9affee6286ea8f5f6d
Nicholas Konar <nkonar@protonmail.com>
2025-02-08 23:18:55 -0500

    cache dataset

--- knn_euclidean.py
+++ knn_euclidean.py
@@ -3,8 +3,31 @@ import numpy as np
 import tensorflow as tf
 
 
+dataset_cache = None
+
+def get_dataset():
+    global dataset_cache
+    if dataset_cache is None:
+        mnist = tf.keras.datasets.mnist
+        (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+        train_images = train_images / 255.0
+        test_images = test_images / 255.0
+        flattened_images = tf.reshape(train_images, [train_images.shape[0], -1])
+        dataset_cache = {
+            'train_images': train_images,
+            'train_labels': train_labels,
+            'test_images': test_images,
+            'test_labels': test_labels,
+            'flattened_images': flattened_images
+        }
+    return dataset_cache
+
 
 def knn(vector, k):
+    data = get_dataset()
+    train_labels = data['train_labels']
+    flattened_images = data['flattened_images']
+    
     diffs = flattened_images - vector
     sq_diffs = tf.square(diffs)
     sums = tf.reduce_sum(sq_diffs, axis=1)
