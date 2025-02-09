2281486ad5eed07b2587a4bf3a9599bff7e90899
Nicholas Konar <nkonar@protonmail.com>
2025-02-08 23:03:17 -0500

    lambda controller

--- knn_euclidean.py
+++ knn_euclidean.py
@@ -1,17 +1,11 @@
+import json
 import numpy as np
 import tensorflow as tf
 
 
-def knn(input_arr, k):
-    mnist = tf.keras.datasets.mnist
-    (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-    train_images = train_images / 255.0
-    test_images = test_images / 255.0
-
-    flattened_images = tf.reshape(train_images, [train_images.shape[0], -1])
-
-    diffs = flattened_images - input_arr
+def knn(vector, k):
+    diffs = flattened_images - vector
     sq_diffs = tf.square(diffs)
     sums = tf.reduce_sum(sq_diffs, axis=1)
     distances = tf.sqrt(sums)
@@ -19,5 +13,21 @@ def knn(input_arr, k):
 
     nearest_labels = [int(train_labels[i]) for i in sorted_dist.numpy()[:k]]
     most_common = np.bincount(nearest_labels).argmax()
-    return most_common
+    return most_common, nearest_labels
 
+
+def lambda_controller(event, context):
+    try:
+        body = json.loads(event['body'])
+        vector = tf.constant(body['vector'], dtype=tf.float32)
+        k = int(body.get('k', 5))
+        result, nearest_labels = knn(vector, k)
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'result': result, 'nearestNeighbors': nearest_labels})
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
